063007d6410bff99abbb4ccded7cea8abf00093d
wei <891001kevin@gmail.com>
2024-04-08 14:12:23 +0800

    4/8 before adding linelogin

--- src/main/java/com/willy/malltest/service/impl/UserServiceImpl.java
+++ src/main/java/com/willy/malltest/service/impl/UserServiceImpl.java
@@ -1,10 +1,11 @@
-package com.willy.malltest.service;
+package com.willy.malltest.service.impl;
 
 import com.willy.malltest.dto.UserDto;
 import com.willy.malltest.model.ThirdParty;
 import com.willy.malltest.model.User;
 import com.willy.malltest.repository.ThirdPartyRepository;
 import com.willy.malltest.repository.UsersRepository;
+import com.willy.malltest.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,7 +17,7 @@ import java.util.Date;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     @Autowired
     private UsersRepository userRepository;
 
